65d3e8165e9e3b59fb91419570b6594e3a2c013a
Salto Sabrina <sabrinasaltoagustina@gmail.com>
2018-05-08 19:29:09 -0300

    Ejercicio 4

---
+++ Ejer_4.java
@@ -0,0 +1,13 @@
+package tarea;
+public class tabla{
+	public static void main(String[] args) {
+		for(int i=5; i<=5; i++) {
+			for(int j =0; j<=50; j++) {
+				System.out.println(i + "*" + j + "=" + i*j);
+			}
+			System.out.println();  
+			}
+		}
+		
+		
+} 
